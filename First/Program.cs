007e47a774391afa6035ef218a1fcc7d2228355b
Aida <aida.zaynullina1@gmail.com>
2017-12-09 14:17:25 +0400

    My third project

--- First/Program.cs
+++ First/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace First
 {
     class Program
     {
         static void Main(string[] args)
+        { 
+                var basket1 = new Fruct_Basket();
+                var basket1_serialized = JsonConvert.SerializeObject(basket1);
+                using (var basketWriter = new StreamWriter(new FileStream("Fruct_Basket.txt", FileMode.OpenOrCreate)))
+                {
+                    basketWriter.WriteLine(basket1_serialized);
+                }
+        }
+
+    }
+        public class Fruct_Basket
         {
+            public string Apple { get; set; } = "Solncem napoyonoe";
+            public string Ananasik { get; set; } = "Pod uzhnym solncem vyros ya";
+            public string Persic { get; set; } = "Chudesniy, dlya zdoroviya polezniy";
+            public string Apelsinсhic { get; set; } = "Sochniy vitaminchik";
         }
     }
-}
---
+++ Second/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Second
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var basket1 = new Basket();
+            using (var basketReader = new StreamReader(new FileStream("Fruct_Basket.txt", FileMode.OpenOrCreate)))
+            {
+                var basket = basketReader.ReadLine();
+                var deserialized_basket = JsonConvert.DeserializeObject<Basket>(basket);
+                Console.WriteLine(JsonConvert.DeserializeObject<Basket>(basket) + "\nPRESS ANY KEY TO EXIT...");
+                Console.ReadKey();
+            }
+        }
+        public class Basket
+        {
+            public string Apple { get; set; } 
+            public string Ananasik { get; set; } 
+            public string Persic { get; set; } 
+            public string Apelsinсhic { get; set; } 
+        }
+    }
+}
